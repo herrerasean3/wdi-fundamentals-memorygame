6990d37a3e03cf47d333988cf4eb551e23f1e1b0
Sean Herrera <herrerasean3@gmail.com>
2017-03-23 20:55:50 -0400

    Completed unit 9 assignment

---
+++ starter-code/js/script.js
@@ -0,0 +1,9 @@
+console.log("JS file is connected to HTML! Woo!");
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+
+if (cardTwo === cardFour) {
+    window.alert("You found a match!");
+}
--- starter-code/main.js
+++
@@ -1,5 +0,0 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = "king"
-var cardTwo = "king"
-var cardThree = "queen"
-var cardFour = "queen"
